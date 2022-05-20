4ff3d39b1d6e021a2e90c9afc0897cb98bcb4049
Téo Chaillou <teo.chaillou@student-cs.fr>
2022-05-21 00:09:06 +0200

    Set calendar in english

--- frontend/src/components/LeftPanel/InterviewCalendar/InterviewCalendar.tsx
+++ frontend/src/components/LeftPanel/InterviewCalendar/InterviewCalendar.tsx
@@ -14,6 +14,7 @@ const InterviewCalendar = ({ selectedDate, setSelectedDate } : CalendarProps) : 
       <Calendar
         onChange={setSelectedDate}
         value={selectedDate}
+        locale="en-EN"
       />
     </div>
   );
